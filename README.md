2dcd7dd2f26772d5c9867d5cefef6bf5fb74a333
Bogdan Nedeljkovic <yesiamtheone111@gmail.com>
2018-04-01 15:27:03 +0200

    Move connect(region, key id and secret) params to command line

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 ### Usage example
 
-python3 vpcaws.py --client_network=10.0.0.0/24 --main_vpc_id=vpc-f2c45099
+Install dependents
+```
+pip3 install -r requirements.txt
+```
 
+Run script
+
+```
+python3 vpcaws.py --client_network=10.0.0.0/24 --main_vpc_id=vpc-f2c45099 --region_name=eu-central-1 --aws_access_key_id=<aws_access_key_id> --aws_secret_access_key=<aws_secret_access_key>
+```
--- vpcaws.py
+++ vpcaws.py
@@ -6,30 +6,41 @@ class ErrCantConnect(Exception):
     pass
 
 class VpcManager:
-    def __init__(self):
-        pass
+    def __init__(self, *,
+                 region_name,
+                 aws_access_key_id,
+                 aws_secret_access_key):
+        self.connect_param= dict(region_name = region_name,
+                                 aws_access_key_id=aws_access_key_id,
+                                 aws_secret_access_key = aws_secret_access_key
+                                 )
 
     async def connect(self):
         try:
-            self.ec2 = boto3.resource('ec2')
+            self.ec2 = boto3.resource('ec2', **self.connect_param)
         except Exception as err:
             raise ErrCantConnect(err)
 
     async def create_vpc(self, *, network):
         self.client_vpc = self.ec2.create_vpc(CidrBlock=network)
-
+        print(self.client_vpc)
         self.subnet = self.client_vpc.create_subnet(CidrBlock=network)
+        print(self.subnet)
         self.client_vpc.wait_until_available()
 
         self.gateway = self.ec2.create_internet_gateway()
+        print(self.gateway)
         self.gateway.attach_to_vpc(VpcId = self.client_vpc.id)
 
     async def connect_vpc_to_main(self, *, vpcid):
         self.ec2.create_vpc_peering_connection(DryRun=False, VpcId=vpcid, PeerVpcId=self.client_vpc.id)
 
-    async def assing_acl(self):
-        pass
-        #self.ec2.create_vpc_peering_connection(DryRun=False, VpcId=vpcid, PeerVpcId=self.client_vpc.id)
+    async def modify_security_group(self):
+        groups = self.ec2.describe_security_groups()
+        for item in groups:
+            print(item)
+
+
 
 
 
@@ -38,13 +49,21 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--client_network", help="Client network specific", required=True)
     parser.add_argument("--main_vpc_id", help="Main VPC id", required=True)
+
+    parser.add_argument("--region_name", help="Region Name", required=True)
+    parser.add_argument("--aws_access_key_id", help="AWS access key Id", required=True)
+    parser.add_argument("--aws_secret_access_key", help="AWS secret access key", required=True)
+
+
     args = parser.parse_args()
 
-    vpcManager = VpcManager()
+    vpcManager = VpcManager(region_name = args.region_name,
+                            aws_access_key_id = args.aws_access_key_id,
+                            aws_secret_access_key=args.aws_secret_access_key)
     loop.run_until_complete(vpcManager.connect())
     loop.run_until_complete(vpcManager.create_vpc(network=args.client_network))
-    loop.run_until_complete(vpcManager.connect_vpc_to_main(vpcid=args.main_vpc_id))
-    #loop.run_until_complete(vpcManager.assing_acl())
+    #loop.run_until_complete(vpcManager.connect_vpc_to_main(vpcid=args.main_vpc_id))
+    loop.run_until_complete(vpcManager.modify_security_group())
     loop.close()
 
 
